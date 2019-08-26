033519ddb1a7bbbecd41d4b93122fc791b835fb7
Paul Miller <jettero@corky.vhb.voltar.org>
2019-08-26 09:53:11 -0400

    fun stuff so far

--- jgpg/cmd.py
+++ jgpg/cmd.py
@@ -3,13 +3,14 @@
 import sys, os
 import click
 
-from jgpg.tools import list_, sign
+from jgpg.tools import list_, msign
 
 @click.command()
+@click.option('--debug', is_flag=True, default=False)
 @click.option('--version', is_flag=True, default=False)
-@click.argument('action', default='sign', type=click.Choice(['sign', 'list', 'help']))
+@click.argument('action', default='msign', type=click.Choice(['msign', 'list', 'help']))
 @click.argument('targets', type=click.Path(exists=True), nargs=-1)
-def run(action, version, targets):
+def run(action, debug, version, targets):
     if version:
         try:
             from jgpg.version import version as vstr
@@ -26,13 +27,20 @@ def run(action, version, targets):
         print(f'{vstr} {sys.argv}')
         sys.exit(0)
 
+    if debug:
+        import logging
+        logging.basicConfig(level=logging.DEBUG)
+
     if action == 'help':
         help()
 
-    elif action == 'sign':
-        sign(targets)
+    elif action == 'msign':
+        msign(targets)
 
     elif action == 'list':
         list_()
+
+    else:
+        raise Exception(f'"{action}" not understood')
 
     sys.exit(0)
--- jgpg/tools.py
+++ jgpg/tools.py
@@ -1,13 +1,49 @@
 # coding: utf-8
 
+import os
+import getpass
+import hashlib
 import gnupg
 
 def list_():
     gpg = gnupg.GPG()
     private_keys = gpg.list_keys(True)
     for keydict in private_keys:
-        uids = ', '.join(keydict['uids'])
-        print(f'uids: {uids}; keyid: {keydict["keyid"]}')
+        joined_uids = ', '.join(keydict['uids'])
+        print('uids: {juids}; keyid: {keyid} (algo={algo})'.format(
+            juids=joined_uids, **keydict))
 
-def sign(targets):
-    print(f'TODO sign: {targets}')
+def _hash_target(fname):
+    s256 = hashlib.sha256()
+    with open(fname, 'rb') as fh:
+        r = fh.read(1024)
+        while r:
+            s256.update(r)
+            r = fh.read(1024)
+    return s256.hexdigest()
+
+def manifest(targets, mfilename='MANIFEST'):
+    with open(mfilename, 'w') as mfh:
+        for fname in targets:
+            if os.path.isfile(fname):
+                digest = _hash_target(fname)
+                mfh.write(f'{digest} {fname}\n')
+            if os.path.isdir(fname):
+                for dirpath, dirnames, filenames in os.walk(fname):
+                    for fname in filenames:
+                        fname_ = os.path.join(dirpath, fname)
+                        digest = _hash_target(fname_)
+                        mfh.write(f'{digest} {fname_}\n')
+
+def _sign_target(fname, ofname):
+    gpg = gnupg.GPG()
+    with open(fname, 'rb') as fh:
+        print(f'dropping {fname} signature in {ofname}')
+        gpg.sign_file(fh, detach=True, binary=False, output=ofname,
+            passphrase=getpass.getpass('passphrase: '))
+
+def msign(targets, mfilename='MANIFEST', sfilename='SIGNATURE'):
+    manifest(targets, mfilename=mfilename)
+    if sfilename in (None, False):
+        sfilename = f'{mfilename}.sig'
+    _sign_target(mfilename, sfilename)
