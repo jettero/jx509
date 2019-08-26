69c29e45a18d090ffeec00bf79a1f48398c7dad5
Paul Miller <jettero@corky.vhb.voltar.org>
2019-08-26 07:54:24 -0400

    skel

---
+++ README.md
@@ -0,0 +1,4 @@
+# Work
+
+I needed to learn to use gpg from python for work … this stupid project is just
+practice for work.
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        import shlex
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+setup(name='gpg',
+    use_scm_version = {
+        'write_to': 'jgpg/version.py',
+        'tag_regex': r'^(?P<prefix>v)(?P<version>\d+\.\d+\.\d+)(?P<suffix>.*)?$',
+        # NOTE: use ./setup.py --version to regenerate version.py and print the
+        # computed version
+    },
+    description   = "jettero's [silly] GnuPG tool",
+    author        = 'Paul Miller',
+    author_email  = 'paul@jettero.pl',
+    url           = 'https://github.com/jettero/jgpg',
+    tests_require = ['pytest',],
+    cmdclass      = {'test': PyTest},
+    packages      = find_packages(),
+    setup_requires = [ 'setuptools_scm' ],
+    install_requires=[
+        'python-gnupg',
+    ],
+    entry_points={
+        'console_scripts': [
+            'jgpg = jgpg:run',
+        ],
+    },
+)
+
