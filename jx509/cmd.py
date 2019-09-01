10956f9c42b0444165aa2f6faa979cc4df0b99e1
Paul Miller <jettero@corky.vhb.voltar.org>
2019-09-01 10:25:28 -0400

    this loader is cute... it's not clear how I'd ever add options... meh?

--- jx509/cmd.py
+++ jx509/cmd.py
@@ -2,13 +2,17 @@
 
 import sys, os
 import click
+import jx509.tools
 
-from jx509.tools import list_, msign, verify
+cmds = ['help']
+for item in dir(jx509.tools):
+    if item.startswith('cmd_') and callable(getattr(jx509.tools,item)):
+        cmds.append(item[4:])
 
 @click.command()
 @click.option('--debug', is_flag=True, default=False)
 @click.option('--version', is_flag=True, default=False)
-@click.argument('action', default='msign', type=click.Choice(['msign', 'list', 'help', 'verify']))
+@click.argument('action', default='msign', type=click.Choice(cmds))
 @click.argument('targets', type=click.Path(exists=True), nargs=-1)
 def run(action, debug, version, targets):
     if version:
@@ -31,17 +35,13 @@ def run(action, debug, version, targets):
         import logging
         logging.basicConfig(level=logging.DEBUG)
 
+    fname = f'cmd_{action}'
+
     if action == 'help':
         help()
 
-    elif action == 'msign':
-        msign(targets)
-
-    elif action == 'verify':
-        verify(targets)
-
-    elif action == 'list':
-        list_()
+    elif hasattr(jx509.tools, fname):
+        getattr(jx509.tools, fname)(targets)
 
     else:
         raise Exception(f'"{action}" not understood')
--- jx509/tools.py
+++ jx509/tools.py
@@ -4,19 +4,10 @@ import os
 import getpass
 import logging
 import hashlib
-import gnupg
 
 log = logging.getLogger('jx509.tools')
 
-def list_():
-    gpg = gnupg.GPG()
-    private_keys = gpg.list_keys(True)
-    for keydict in private_keys:
-        joined_uids = ', '.join(keydict['uids'])
-        print('uids: {juids}; keyid: {keyid} (algo={algo})'.format(
-            juids=joined_uids, **keydict))
-
-def _hash_target(fname):
+def hash_target(fname):
     s256 = hashlib.sha256()
     with open(fname, 'rb') as fh:
         r = fh.read(1024)
@@ -27,7 +18,12 @@ def _hash_target(fname):
     log.debug(f'hashed {fname}: {hd}')
     return hd
 
-def _descend_targets(targets, cb):
+def cmd_hash(targets):
+    for target in targets:
+        hash = hash_target(target)
+        print(f'{hash}  {target}')
+
+def descend_targets(targets, cb):
     for fname in targets:
         if os.path.isfile(fname):
             cb(fname)
@@ -40,45 +36,48 @@ def _descend_targets(targets, cb):
 def manifest(targets, mfilename='MANIFEST'):
     with open(mfilename, 'w') as mfh:
         def append_hash(fname):
-            digest = _hash_target(fname)
+            digest = hash_target(fname)
             mfh.write(f'{digest} {fname}\n')
-        _descend_targets(targets, append_hash)
+        descend_targets(targets, append_hash)
 
-def _sign_target(fname, ofname):
-    gpg = gnupg.GPG()
-    with open(fname, 'rb') as fh:
-        print(f'dropping {fname} signature in {ofname}')
-        gpg.sign_file(fh, detach=True, binary=False, output=ofname,
-            passphrase=getpass.getpass('passphrase: '))
+cmd_manifest = manifest
 
-def msign(targets, mfilename='MANIFEST', sfilename='SIGNATURE'):
+# def sign_target(fname, ofname, cert='private.pem'):
+#     gpg = gnupg.GPG()
+#     with open(fname, 'rb') as fh:
+#         print(f'dropping {fname} signature in {ofname}')
+#         gpg.sign_file(fh, detach=True, binary=False, output=ofname,
+#             passphrase=getpass.getpass('passphrase: '))
+
+def cmd_msign(targets, mfilename='MANIFEST', sfilename='SIGNATURE'):
     manifest(targets, mfilename=mfilename)
     if sfilename in (None, False):
         sfilename = f'{mfilename}.sig'
-    _sign_target(mfilename, sfilename)
+#   sign_target(mfilename, sfilename)
 
-def verify(targets, mfname='MANIFEST', sfname='SIGNATURE'):
-    gpg = gnupg.GPG()
-    if not sfname:
-        if os.path.isfile('SIGNATURE'):
-            sfname = 'SIGNATURE'
-        elif os.path.isfile(f'{mfname}.sig'):
-            sfname = f'{mfname}.sig'
-        elif os.path.isfile(f'{mfname}.asc'):
-            sfname = f'{mfname}.asc'
-    with open(sfname, 'rb') as sfh:
-        verified = gpg.verify_file(sfh, mfname)
-    if verified.trust_level is not None and verified.trust_level >= verified.TRUST_FULLY:
-        hash_dict = dict()
-        with open(mfname, 'r') as mfh:
-            for line in mfh:
-                hash,fname = line.strip().split()
-                hash_dict[fname] = hash
-        def verify_hash(fname):
-            digest = _hash_target(fname)
-            if digest != hash_dict[fname]:
-                raise Exception(f'hash for {fname} does not match signed manifest:\n'
-                    f'     {digest}\n  vs {hash_dict[fname]}')
-        _descend_targets(targets, verify_hash)
-    else:
-        raise Exception(f'{mfname} does not match signature in {sfname} or is otherwise not trusted')
+
+# def verify(targets, mfname='MANIFEST', sfname='SIGNATURE'):
+#     gpg = gnupg.GPG()
+#     if not sfname:
+#         if os.path.isfile('SIGNATURE'):
+#             sfname = 'SIGNATURE'
+#         elif os.path.isfile(f'{mfname}.sig'):
+#             sfname = f'{mfname}.sig'
+#         elif os.path.isfile(f'{mfname}.asc'):
+#             sfname = f'{mfname}.asc'
+#     with open(sfname, 'rb') as sfh:
+#         verified = gpg.verify_file(sfh, mfname)
+#     if verified.trust_level is not None and verified.trust_level >= verified.TRUST_FULLY:
+#         hash_dict = dict()
+#         with open(mfname, 'r') as mfh:
+#             for line in mfh:
+#                 hash,fname = line.strip().split()
+#                 hash_dict[fname] = hash
+#         def verify_hash(fname):
+#             digest = hash_target(fname)
+#             if digest != hash_dict[fname]:
+#                 raise Exception(f'hash for {fname} does not match signed manifest:\n'
+#                     f'     {digest}\n  vs {hash_dict[fname]}')
+#         descend_targets(targets, verify_hash)
+#     else:
+#         raise Exception(f'{mfname} does not match signature in {sfname} or is otherwise not trusted')
