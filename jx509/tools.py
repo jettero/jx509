85a17157f8a983b16203ac110706097c53fdb3da
Paul Miller <jettero@corky.vhb.voltar.org>
2019-09-15 09:37:47 -0400

    this stuff seems actually to work... it's not exactly straight forward though

--- jx509/tools.py
+++ jx509/tools.py
@@ -6,10 +6,13 @@ import logging
 import re
 import json
 
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
+from Crypto.IO import PEM
+from Crypto.Util import asn1
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
+import OpenSSL.crypto as ossl
 
 log = logging.getLogger(__name__)
 
@@ -18,6 +21,43 @@ class STATUS:
     FAIL = 'fail'
     VERIFIED = 'verified'
     UNKNOWN = 'unknown'
+
+class X509:
+    PEMData = namedtuple('PEMData', ['cert','marker','decrypted'])
+    CrtData = namedtuple('CrtData', ['cert','algo','sig'])
+    SigData = namedtuple('SigData', ['nonzero_bits','dat', 'algo'])
+
+    def __init__(self, crt_fname, ca_fname):
+        with open(ca_fname, 'r') as fh:
+            self.ca_raw = fh.read()
+        with open(crt_fname, 'r') as fh:
+            self.crt_raw = fh.read()
+
+        self.ca = ossl.load_certificate(ossl.FILETYPE_PEM, self.ca_raw)
+
+        ossl_crt = ossl.load_certificate(ossl.FILETYPE_PEM, self.crt_raw)
+        self.pdat = self.PEMData(*PEM.decode(self.crt_raw))
+        self.cdat = self.CrtData(*asn1.DerSequence().decode(self.pdat.cert))
+        _sig = asn1.DerObject().decode(self.cdat.sig).payload
+        self.sig = self.SigData(_sig[0], _sig[1:], ossl_crt.get_signature_algorithm().decode())
+        self.crt = RSA.importKey(self.pdat.cert)
+        self.verifier = PKCS1_v1_5.new(self.crt)
+
+    def verify_cert(self):
+        if self.sig.nonzero_bits != 0:
+            e = Exception("sig invalidated by strange nzb")
+            log.error('The CA cert (%s) may not approve of this certificate (%s): %s',
+                ca_fname, crt_fname, e)
+            return (False, e)
+
+        try:
+            ossl.verify(self.ca, self.sig.dat, self.cdat.cert, self.sig.algo)
+            return True
+        except ossl.Error as oce:
+            log.error('The CA cert (%s) does not seem to approve of this certificate (%s): %s',
+                ca_fname, crt_fname, oce)
+            return (False, oce)
+        return # <--- this can't happen, right?
 
 MANIFEST_RE = re.compile(r'^\s*(?P<digest>[0-9a-fA-F]+)\s+(?P<fname>.+)$')
 
@@ -81,18 +121,20 @@ def sign_target(fname, ofname, key_file='private.key'):
         fh.write(RSA.PEM.encode(sig, f'Detached Signature of {fname}'))
         fh.write('\n')
 
-def verify_signature(fname, sfname, cert_file='public.crt'):
-    with open(cert_file, 'r') as fh:
-        public_cert = RSA.importKey(fh.read())
-    verifier = PKCS1_v1_5.new(public_cert)
+def verify_signature(fname, sfname, cert_file='public.crt', ca_fname='ca-root.crt'):
+    x509 = X509(cert_file, ca_fname)
+    if not x509.verify_cert():
+        log.error('bad certificate: %s; not checking any files against it', cert_file)
+        return STATUS.FAIL
     try:
         with open(sfname, 'r') as fh:
             signature,_,_ = RSA.PEM.decode(fh.read()) # also returns header and decrypted-status
     except FileNotFoundError:
         log.error('failed to find %s for %s', sfname, fname)
         return STATUS.UNKNOWN
-    if verifier.verify(hash_target(fname, obj_mode=True), signature):
+    if x509.verifier.verify(hash_target(fname, obj_mode=True), signature):
         return STATUS.VERIFIED
+    log.error('%s failed signature check (%s)', fname, sfname)
     return STATUS.FAIL
 
 def verify_files(targets, mfname='MANIFEST', sfname='SIGNATURE', cert_file='public.crt', output_json=False):
